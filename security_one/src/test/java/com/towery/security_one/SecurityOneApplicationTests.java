9807988014901c70f641f64c6d92e7e930d8a318
lyq <3471966565@qq.com>
2023-03-11 18:02:57 +0800

    Initial commit

---
+++ security_one/src/test/java/com/towery/security_one/SecurityOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.towery.security_one;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityOneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
